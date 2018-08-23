32a4abf6fddfbdf8163c6a1897afb3d3256dd42e
justcallmebeef <bethanymitch10@gmail.com>
2018-08-23 15:43:39 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var button1 = document.getElementById("buttonbutton")
+var paragraph = document.getElementById("paragraph")
+button1.addEventListener("click", newButton)
+function newButton(){
+    paragraph.textContent = "A Croak-A-Cola!"
+}
